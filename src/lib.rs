6068304f12edeffbe0cba6f5a53b55d2d987bab7
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2023-10-20 19:33:37 +0200

    fix: use Path fro Unix and str for Windows

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,29 @@
 mod common;
 mod platform;
 
+use std::path::PathBuf;
 use common::utils::absolute;
 
 #[cfg(target_os = "windows")]
 use platform::windows::reflink_sync;
 
-#[cfg(target_os = "linux")]
-use reflink_copy::reflink as reflink_sync;
-
-#[cfg(target_os = "macos")]
+#[cfg(not(target_os = "windows"))]
 use reflink_copy::reflink as reflink_sync;
 
 pub fn reflink_file_sync(src: &str, dest: &str) -> std::io::Result<()> {
-    reflink_sync(
-        absolute(src)?.to_str().unwrap_or_default(),
-        absolute(dest)?.to_str().unwrap_or_default(),
-    )
+    // Convert to absolute paths
+    let src_abs: PathBuf = absolute(src)?;
+    let dest_abs: PathBuf = absolute(dest)?;
+
+    #[cfg(target_os = "windows")]
+    {
+        let src_str = src_abs.to_str().unwrap_or_default();
+        let dest_str = dest_abs.to_str().unwrap_or_default();
+        reflink_sync(src_str, dest_str)
+    }
+
+    #[cfg(not(target_os = "windows"))]
+    {
+        reflink_sync(&src_abs, &dest_abs)
+    }
 }
