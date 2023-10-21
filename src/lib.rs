7721e2248028dcdf1c547758294f0decb8696caf
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2023-10-21 18:48:19 +0200

    test: add tests in lib
    Signed-off-by: Ignacio Aldama Vicente <sr.drabx@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -27,3 +27,36 @@ pub fn reflink_file_sync(src: &str, dest: &str) -> std::io::Result<()> {
         reflink_sync(&src_abs, &dest_abs)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::File;
+    use std::io::Write;
+    use tempfile::Builder;
+
+    fn create_file(path: &str) {
+        let mut file = File::create(path).unwrap();
+        file.write_all(b"Hello, world!").unwrap();
+    }
+
+    #[test]
+    fn test_reflink_file_sync() {
+        // Create a temporary directory inside the Current Working Directory
+        let tmp_dir = Builder::new()
+            .prefix("test_reflink_file_sync")
+            .tempdir()
+            .unwrap();
+        let src = tmp_dir.path().join("test_reflink_file_sync_src");
+        let dest = tmp_dir.path().join("test_reflink_file_sync_dest");
+
+        // Create a file
+        create_file(src.to_str().unwrap());
+
+        // Reflink the file
+        reflink_file_sync(src.to_str().unwrap(), dest.to_str().unwrap()).unwrap();
+
+        // Cleanup
+        tmp_dir.close().unwrap();
+    }
+}
