cf84bde195cee8b44637984a9f974512a857e5ea
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2023-10-22 12:38:40 +0200

    fix: use "code" as tests dir

--- init-script.sh
+++ init-script.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
 # Update the package list and install essential packages
+echo "::group::Update the package list and install essential packages"
 sudo apt-get update
 sudo apt-get install -y build-essential git curl
+echo "::endgroup::"
 
 # Install Rust and Cargo
+echo "::group::Install Rust and Cargo"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
+echo "::endgroup::"
 
 # Prepare Disk
+echo "::group::Prepare Disk"
 sudo mkdir -p /mnt/btrfs
 sudo mkfs.btrfs /dev/sdb
 sudo mount /dev/sdb /mnt/btrfs
+echo "::endgroup::"
 
 # Prepare User
+echo "::group::Prepare User"
 sudo useradd -m ghaction
+echo "::endgroup::"
 
 # SSH Setup for the new user
+echo "::group::SSH Setup for the new user"
 sudo mkdir -p ~ghaction/.ssh
 sudo touch ~ghaction/.ssh/authorized_keys
 sudo chown -R ghaction:ghaction ~ghaction/.ssh
 sudo chmod 700 ~ghaction/.ssh
 sudo chmod 600 ~ghaction/.ssh/authorized_keys
+echo "::endgroup::"
 
 # Ensure that the user has access to the btrfs volume
 sudo chown ghaction:ghaction /mnt/btrfs
