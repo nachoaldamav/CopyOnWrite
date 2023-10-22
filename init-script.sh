fa9082e6d7670c83f5f97c100f4c6ed5fca0fd7f
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2023-10-22 10:30:29 +0200

    fix: ensure the new user has permissions

--- init-script.sh
+++ init-script.sh
@@ -7,3 +7,9 @@ sudo apt-get install -y build-essential git curl
 # Install Rust and Cargo
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
+
+# Ensure that the user has access to the btrfs volume
+sudo mkdir -p /mnt/btrfs
+sudo mount /dev/sdb /mnt/btrfs
+sudo mkdir /home/ghaction/.ssh
+sudo chown ghaction:ghaction /home/ghaction/.ssh
