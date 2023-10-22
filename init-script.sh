5afcb43ab4a65ced604cab88e47ed9139dbcda6e
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2023-10-22 10:59:37 +0200

    fix: process ssh dirs inside init script

--- init-script.sh
+++ init-script.sh
@@ -8,9 +8,20 @@ sudo apt-get install -y build-essential git curl
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
-# Ensure that the user has access to the btrfs volume
+# Prepare Disk
 sudo mkdir -p /mnt/btrfs
+sudo mkfs.btrfs /dev/sdb
 sudo mount /dev/sdb /mnt/btrfs
-sudo mkdir /home/ghaction/.ssh
-sudo chown ghaction:ghaction /home/ghaction/.ssh
-sudo mkdir /home/ghaction/.ssh/authorized_keys
+
+# Prepare User
+sudo useradd -m ghaction
+
+# SSH Setup for the new user
+sudo mkdir -p ~ghaction/.ssh
+sudo touch ~ghaction/.ssh/authorized_keys
+sudo chown -R ghaction:ghaction ~ghaction/.ssh
+sudo chmod 700 ~ghaction/.ssh
+sudo chmod 600 ~ghaction/.ssh/authorized_keys
+
+# Ensure that the user has access to the btrfs volume
+sudo chown ghaction:ghaction /mnt/btrfs
