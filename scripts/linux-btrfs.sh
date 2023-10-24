fe383e61bead9db2e13e0fd01d3a6ceeed766a39
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2023-10-24 21:50:27 +0200

    fix: dont change owner of new partition

--- scripts/linux-btrfs.sh
+++ scripts/linux-btrfs.sh
@@ -7,23 +7,5 @@ sudo mkfs.btrfs /dev/sdb
 sudo mount /dev/sdb /mnt/btrfs
 echo "::endgroup::"
 
-# Prepare User
-echo "::group::Prepare User"
-sudo useradd -m ghaction
-echo "::endgroup::"
-
-# SSH Setup for the new user
-echo "::group::SSH Setup for the new user"
-sudo mkdir -p ~ghaction/.ssh
-sudo touch ~ghaction/.ssh/authorized_keys
-sudo chown -R ghaction:ghaction ~ghaction/.ssh
-sudo chmod 700 ~ghaction/.ssh
-sudo chmod 600 ~ghaction/.ssh/authorized_keys
-echo "::endgroup::"
-
-# Ensure that the user has access to the btrfs volume
-sudo chown ghaction:ghaction /mnt/btrfs
-
 # Create "code" directory in the btrfs volume
 sudo mkdir -p /mnt/btrfs/code
-sudo chown ghaction:ghaction /mnt/btrfs/code
--- scripts/linux-xfs.sh
+++ scripts/linux-xfs.sh
@@ -8,23 +8,5 @@ sudo mount /dev/sdb /mnt/xfs
 sudo xfs_io -c 'reflink on' /mnt/xfs
 echo "::endgroup::"
 
-# Prepare User
-echo "::group::Prepare User"
-sudo useradd -m ghaction
-echo "::endgroup::"
-
-# SSH Setup for the new user
-echo "::group::SSH Setup for the new user"
-sudo mkdir -p ~ghaction/.ssh
-sudo touch ~ghaction/.ssh/authorized_keys
-sudo chown -R ghaction:ghaction ~ghaction/.ssh
-sudo chmod 700 ~ghaction/.ssh
-sudo chmod 600 ~ghaction/.ssh/authorized_keys
-echo "::endgroup::"
-
-# Ensure that the user has access to the xfs volume
-sudo chown ghaction:ghaction /mnt/xfs
-
 # Create "code" directory in the xfs volume
 sudo mkdir -p /mnt/xfs/code
-sudo chown ghaction:ghaction /mnt/xfs/code
