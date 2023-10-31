429b692fb5a0dba9e7e80cdc228859ce2314c556
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2023-10-31 13:42:28 +0100

    fix: change init script for ext4

--- scripts/linux-ext4.sh
+++ scripts/linux-ext4.sh
@@ -2,27 +2,11 @@
 
 # Prepare Disk
 echo "::group::Prepare Disk"
-
-# Unmount the disk if it's mounted
-sudo umount /dev/sdb1 2>/dev/null || true
-
-# Create a new ext4 filesystem on the drive
-sudo mkfs.ext4 /dev/sdb1
-
-# Create a directory to serve as the mount point
 sudo mkdir -p /mnt/ext4
-
-# Mount the drive
-sudo mount /dev/sdb1 /mnt/ext4
-
+sudo mkfs.ext4 /dev/sdb
+sudo mount /dev/sdb /mnt/ext4
 echo "::endgroup::"
 
-# Create a "code" directory in the ext4 volume
 sudo mkdir -p /mnt/ext4/code
-
-# Give ownership to the current user
 sudo chown -R $USER:$USER /mnt/ext4/code
-
-# Give full permissions to the current user
 sudo chmod -R 777 /mnt/ext4/code
-
